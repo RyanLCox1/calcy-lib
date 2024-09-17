62abb475cb2b8066815c9bd3d10186978d132cc2
Ryan Cox <ryan@smarty.com>
2024-09-17 10:34:37 -0600

    Add subtraction, multiplication, and division.

--- calc/calc.go
+++ calc/calc.go
@@ -2,7 +2,22 @@ package calc
 
 type Addition struct {
 }
+type Subtraction struct{}
+type Division struct{}
+type Multiplication struct{}
 
 func (this Addition) Calculate(a, b int) int {
 	return a + b
+}
+
+func (this Subtraction) Calculate(a, b int) int {
+	return a - b
+}
+
+func (this Division) Calculate(a, b int) int {
+	return a / b
+}
+
+func (this Multiplication) Calculate(a, b int) int {
+	return a * b
 }
--- calc/calc_test.go
+++ calc/calc_test.go
@@ -10,6 +10,22 @@ func TestAddition(t *testing.T) {
 	assertEqual(t, 0, a.Calculate(0, 0))
 }
 
+func TestSubtraction(t *testing.T) {
+	s := Subtraction{}
+	assertEqual(t, 1, s.Calculate(2, 1))
+	assertEqual(t, -7, s.Calculate(3, 10))
+	assertEqual(t, -3, s.Calculate(-2, 1))
+	assertEqual(t, 0, s.Calculate(0, 0))
+}
+
+func TestMultiplication(t *testing.T) {
+	m := Multiplication{}
+	assertEqual(t, 2, m.Calculate(2, 1))
+	assertEqual(t, 30, m.Calculate(3, 10))
+	assertEqual(t, -2, m.Calculate(-2, 1))
+	assertEqual(t, 0, m.Calculate(0, 0))
+}
+
 func assertEqual(t *testing.T, a, b any) {
 	if a != b {
 		t.Helper()
